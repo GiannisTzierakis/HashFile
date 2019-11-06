df9f487e4ed5dfc830b98df8cd75febbbf06e4ec
GiannnisTz <45824529+GiannnisTz@users.noreply.github.com>
2019-11-06 16:06:43 +0200

    Add files via upload

---
+++ hashfile/Makefile
@@ -0,0 +1,11 @@
+hp:
+	@echo " Compile ht_main ...";
+	gcc -I ./include/ -L ./lib/ -Wl,-rpath,./lib/ ./examples/ht_main.c ./src/hash_file.c -lbf -o ./build/runner -O2
+
+bf:
+	@echo " Compile bf_main ...";
+	gcc -I ./include/ -L ./lib/ -Wl,-rpath,./lib/ ./examples/bf_main.c -lbf -o ./build/runner -O2
+
+ht1:
+	@echo " Compile ht_main ...";
+	gcc -I ./include/ -L ./lib/ -Wl,-rpath,./lib/ ./examples/main1.c ./src/hash_file.c -lbf -o ./build/runner -O2
---
+++ hashfile/examples/main1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "bf.h"
+#include "hash_file.h"
+
+#define RECORDS_NUM 1700 // you can change it if you want
+#define BUCKETS_NUM 13  // you can change it if you want
+#define FILE_NAME "data.db"
+
+int *open_files;
+
+const char* names[] = {
+  "Yannis",
+  "Christofos",
+  "Sofia",
+  "Marianna",
+  "Vagelis",
+  "Maria",
+  "Iosif",
+  "Dionisis",
+  "Konstantina",
+  "Theofilos",
+  "Giorgos",
+  "Dimitris"
+};
+
+const char* surnames[] = {
+  "Ioannidis",
+  "Svingos",
+  "Karvounari",
+  "Rezkalla",
+  "Nikolopoulos",
+  "Berreta",
+  "Koronis",
+  "Gaitanis",
+  "Oikonomou",
+  "Mailis",
+  "Michas",
+  "Halatsis"
+};
+
+const char* cities[] = {
+  "Athens",
+  "San Francisco",
+  "Los Angeles",
+  "Amsterdam",
+  "London",
+  "New York",
+  "Tokyo",
+  "Hong Kong",
+  "Munich",
+  "Miami"
+};
+
+#define CALL_OR_DIE(call)     \
+  {                           \
+    HT_ErrorCode code = call; \
+    if (code != HT_OK) {      \
+      printf("Error\n");      \
+      exit(code);             \
+    }                         \
+  }
+
+int main(void){
+  
+  int indexDesc;
+
+  BF_Init(LRU);
+  CALL_OR_DIE(HT_Init());
+  
+  CALL_OR_DIE(HT_CreateIndex(FILE_NAME,BUCKETS_NUM));
+  CALL_OR_DIE(HT_OpenIndex(FILE_NAME, &indexDesc));
+  
+  CALL_OR_DIE(HT_CloseFile(indexDesc));
+
+  return 0;
+}
